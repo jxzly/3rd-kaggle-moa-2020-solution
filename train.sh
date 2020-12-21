7956ac36cef73c1f6392323c799e1ec309ab43f7
daishu <jxzly1993@gmail.com>
2020-12-22 00:15:50 +0800

    update

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-#python ./daishu_solution.py --input_dir $1
-#python ./shiji_solution1.py --input_dir $1
-#python ./shiji_solution2.py --input_dir $1
-#python ./shiji_solution3.py --input_dir $1
+python ./daishu_solution.py --input_dir $1
+python ./shiji_solution1.py --input_dir $1
+python ./shiji_solution2.py --input_dir $1
+python ./shiji_solution3.py --input_dir $1
 python ./blend.py
